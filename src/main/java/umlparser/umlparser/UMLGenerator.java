aa52b86be4e0e4aae10f8c1eab5533e2fcf511d3
Swati Gupta <swati.gupta@sjsu.edu>
2017-02-24 20:50:06 -0800

    Completed: Method to create the UML diagram using yuml.
    Hard coded the grammar for now to test the yuml. Wrote a method which
    accepts the grammar and the fully qualified output file name and creates
    a .png file(UML diagram) at that location.

--- src/main/java/umlparser/umlparser/UMLGenerator.java
+++ src/main/java/umlparser/umlparser/UMLGenerator.java
@@ -5,7 +5,11 @@ package umlparser.umlparser;
 
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,7 +25,7 @@ import com.github.javaparser.ast.body.TypeDeclaration;
  * @author Swati Gupta San Jose State University Spring 2017 CMPE 202
  */
 public class UMLGenerator {
-	
+
 	JavaModel javaModel;
 
 	/**
@@ -38,8 +42,12 @@ public class UMLGenerator {
 		createCompilationUnit(inputFilesPath);
 		createClassOrInterfaceMap(javaModel.getCompilationUnitArrayList());
 		for (CompilationUnit cu : javaModel.getCompilationUnitArrayList()) {
-			grammar += createGrammar(cu);
-		}		
+			// grammar += createGrammar(cu);
+			grammar = createGrammar(cu);
+		}
+
+		String fullyQualifiedOutputFileName = inputFilesPath + "\\" + outputFileName + ".png";
+		createUMLDiagram(grammar, fullyQualifiedOutputFileName);
 	}
 
 	// Method to create a compilation unit
@@ -73,18 +81,46 @@ public class UMLGenerator {
 			for (Node node : typeDeclarationList) {
 				ClassOrInterfaceDeclaration coi = (ClassOrInterfaceDeclaration) node;
 				// coi.isInterface(): false --> class, true --> interface
-				classOrInterfaceMap.put(coi.getName(), coi.isInterface()); 
+				classOrInterfaceMap.put(coi.getName(), coi.isInterface());
 			}
 		}
 		javaModel.setClassOrInterfaceMap(classOrInterfaceMap);
 	}
-	
+
+	// Method to create the grammar
 	private String createGrammar(CompilationUnit cu) {
 		String grammar = "";
-		
-		
-		
+
+		grammar = "[<<interface>>;A1]," + "[<<interface>>;A2]," + "[B1]," + "[B1] -^ [P],"
+				+ "[B1] -.-^ [<<interface>>;A1]," + "[B2],[B2] -^ [P]," + "[B2] -.-^ [<<interface>>;A1],"
+				+ "[B2] -.-^ [<<interface>>;A2]," + "[C1|+ test(a1 : A1) : void]," + "[C1] uses -.->[<<interface>>;A1],"
+				+ "[C2|+ test(a2 : A2) : void]," + "[C2] uses -.->[<<interface>>;A2]," + "[P]";
+
 		return grammar;
+	}
+
+	// Method to create the UML diagram and
+	// create a .png file of the diagram at the test cases location
+	public void createUMLDiagram(String grammar, String fullyQualifiedOutputFileName) {
+		String yumlURLString = "https://yuml.me/diagram/scruffy/class/" + grammar + ".png";
+		int count = 0;
+		byte[] bufferArray = new byte[1024];
+		try {
+
+			URL url = new URL(yumlURLString);
+			HttpURLConnection conn = (HttpURLConnection) url.openConnection();
+			conn.setRequestMethod("GET");
+			conn.setRequestProperty("Accept", "application/json");
+			OutputStream outputStream = new FileOutputStream(new File(fullyQualifiedOutputFileName));
+
+			while ((count = conn.getInputStream().read(bufferArray)) != -1) {
+				outputStream.write(bufferArray, 0, count);
+			}
+			outputStream.close();
+			conn.disconnect();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 
 }
