029e4a606eb476f1e81fcec5ee83f72a5c96b979
Swati Gupta <swati.gupta@sjsu.edu>
2017-04-10 21:38:22 -0700

    Bug fix in UMLParser.java file

--- src/main/java/umlparser/umlparser/UMLParser.java
+++ src/main/java/umlparser/umlparser/UMLParser.java
@@ -8,33 +8,42 @@ import java.io.IOException;
 import com.github.javaparser.ParseException;
 
 /**
- * @author Swati Gupta
- * San Jose State University
- * Spring 2017
- * CMPE 202
+ * @author Swati Gupta San Jose State University Spring 2017 CMPE 202
  */
 public class UMLParser {
 
 	/**
 	 * @param args
-	 * @throws ParseException 
-	 * @throws IOException 
+	 * @throws ParseException
+	 * @throws IOException
 	 */
 	public static void main(String[] args) throws IOException, ParseException {
 		// User inputs
 		String inputFilesPath = args[0];
 		String outputFileName = args[1];
-		String className = args[2];
-		String functionName = args[3];
+		String className = "";
+		String functionName = "";
 		
-		if (inputFilesPath.length() > 0 && outputFileName.length() > 0 && className.isEmpty() && functionName.isEmpty()) {
+		if (args.length > 2) {
+			 if (null != args[2]) {
+			 className = args[2];
+			 }
+			
+			 if (null != args[3]) {
+			 functionName = args[3];
+			 }
+		}
+
+		if (inputFilesPath.length() > 0 && outputFileName.length() > 0 && className.isEmpty()
+				&& functionName.isEmpty()) {
 			// Invoke method to generate the UML diagram
 			UMLGenerator umlGenerator = new UMLGenerator();
-			umlGenerator.generate(inputFilesPath, outputFileName);			
-		} else if (inputFilesPath.length() > 0 && outputFileName.length() > 0 && className.length() > 0 && functionName.length() > 0) {
+			umlGenerator.generate(inputFilesPath, outputFileName);
+		} else if (inputFilesPath.length() > 0 && outputFileName.length() > 0 && className.length() > 0
+				&& functionName.length() > 0) {
 			// Invoke method to generate the Sequence diagram
 			SequenceGenerator sequenceGenerator = new SequenceGenerator();
-			sequenceGenerator.generate(inputFilesPath, outputFileName, className, functionName);			
+			sequenceGenerator.generate(inputFilesPath, outputFileName, className, functionName);
 		} else {
 			System.out.println("Incorrect arguments. Please enter the arguments correctly.");
 		}
